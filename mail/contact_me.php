474d98628098b600539515f152dbb1bb68898ce4
nehabo <neha.bommareddy@gmail.com>
2016-12-16 20:44:03 +0530

    updates contact form using formspree.io

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -15,6 +15,7 @@ $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
+
 // Create the email and send the message
 $to = 'neha.bommareddy@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
